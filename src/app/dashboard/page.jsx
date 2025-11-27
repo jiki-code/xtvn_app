2a55416bc358a9ae9a6230e0b7eac296a6a88352
millie <millie@xtvn88.com>
2025-11-27 16:21:45 +0700

    update

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -131,7 +131,6 @@ export default function HomePage() {
         {isOnBreak ? "Break Out" : "Break In"}
       </DashboardButton>
 
-
       <DashboardButton
         type="checkout"
         onClick={handleCheckOut}
